92cd7fce68ad86ea4ab4a104e109148300cbc20d
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-09-24 07:16:57 +0530

    Add project files.

---
+++ Code Challenge_Day 29 Bitwise AND/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Code_Challenge_Day_29_Bitwise_AND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
